683a248944829a632113a5dff665d19659b8199e
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-10-24 18:01:17 +0200

    try to escape $s

--- src/main/g8/pdf.sh
+++ src/main/g8/pdf.sh
@@ -4,9 +4,9 @@
 # $2 = the output file name you want
 set -x
 # use the latex template
-pandoc $1 -o $2.tex --template=./era7.report.tex \
+pandoc \$1 -o \$2.tex --template=./era7.report.tex \
   --variable project=$project$
   --latex-engine=xelatex --natbib --smart --toc --reference-links  
 # now run xelatex on it
-xelatex $2.tex
-xelatex $2.tex
+xelatex \$2.tex
+xelatex \$2.tex
