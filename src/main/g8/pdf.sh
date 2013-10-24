71b49224691d54c62442c87a1fd3f2d6e1eabee4
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-10-24 17:54:39 +0200

    init repo, first try

---
+++ src/main/g8/pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this script will generate
+# $1 = your markdown file
+# $2 = the output file name you want
+set -x
+# use the latex template
+pandoc $1 -o $2.tex --template=./era7.report.tex \
+  --variable project=$project$
+  --latex-engine=xelatex --natbib --smart --toc --reference-links  
+# now run xelatex on it
+xelatex $2.tex
+xelatex $2.tex
---
+++ src/main/g8/sample.md
@@ -0,0 +1,14 @@
+% title
+% author1
+  author2
+  author3
+% date
+
+# first section
+
+Hola!
+
+## subsection
+
+- a list
+- with two items
