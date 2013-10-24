de4618caf4c7743db4f61ea6551a065a441fa7d1
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2013-10-24 18:10:59 +0200

    fix mode and script stuff

--- src/main/g8/pdf.sh
+++ src/main/g8/pdf.sh
@@ -2,11 +2,11 @@
 # this script will generate
 # \$1 = your markdown file
 # \$2 = the output file name you want
-set -x
+set -e
 # use the latex template
 pandoc \$1 -o \$2.tex --template=./era7.report.tex \
   --variable project=$project$
-  --latex-engine=xelatex --natbib --smart --toc --reference-links  
+  --latex-engine=xetex --natbib --smart --toc --reference-links  
 # now run xelatex on it
 xelatex \$2.tex
 xelatex \$2.tex
